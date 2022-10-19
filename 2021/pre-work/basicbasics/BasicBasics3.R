a23c7bccf561a9f54a1041671f22ef1f3f797bb2
jennysloane <45774310+jennysloane@users.noreply.github.com>
2022-10-19 17:58:43 -0500

    add pre-work

---
+++ 2021/pre-work/basicbasics/BasicBasics3.R
@@ -0,0 +1,22 @@
+
+# Hi Student,
+
+## This assignment is for you to practice some coding before our course and is based on the RyouWithMe unit: BasicBasics 3. 
+
+# In this assignment, you will be asked to read in some data and to look into its structure.
+# To do this:
+
+# 1. Please go to your files and see a dataset name: data_choiceAge_Group.csv 
+# 2. save is as a new variable : MyFirstDataSet (e.g. ).
+
+# 3. Show below that you can view the dataset and look at the dimensions/structure by:
+# 3.1. Indicate how many observations are there in the data set?
+
+# 3.2. Indicate how many variables?
+
+# 3.3. What type is the column: IQ?
+
+# 3.4. What is the name of the function that can show you the only the upper part of the dataset in the console (i.e. in case you just want to have a quick look at the first rows)?  
+
+
+# 4. What is the Mean and Max IQ values in the dataset? 
